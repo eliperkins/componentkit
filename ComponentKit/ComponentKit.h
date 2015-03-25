d3904428204ca1719b80cc072045f29f9c35aca1
Eli Perkins <eli.perkins@venmo.com>
2015-03-25 16:52:06 -0700

    Add framework targets for ComponentKit and ComponentTextKit

---
+++ ComponentKit/ComponentKit.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for ComponentKit.
+FOUNDATION_EXPORT double ComponentKitVersionNumber;
+
+//! Project version string for ComponentKit.
+FOUNDATION_EXPORT const unsigned char ComponentKitVersionString[];
---
+++ ComponentTextKit/ComponentTextKit.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for ComponentTextKit.
+FOUNDATION_EXPORT double ComponentTextKitVersionNumber;
+
+//! Project version string for ComponentTextKit.
+FOUNDATION_EXPORT const unsigned char ComponentTextKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ComponentTextKit/PublicHeader.h>
+
+
